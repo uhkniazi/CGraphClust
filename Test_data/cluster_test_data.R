ddda56def6fa84b6f795408706beb6bedb4e7e42
uhkniazi <uhkniazi@users.noreply.github.com>
2015-08-10 12:35:05 +0100

    changes to test run to produce results as csv files with gene summaries

--- Test_data/cluster_test_data.R
+++ Test_data/cluster_test_data.R
@@ -72,17 +72,47 @@ plot.centrality.diagnostics(oGr)
 # get the centrality parameters
 mCent = mPrintCentralitySummary(oGr)
 # top  5% of the vertices from each category and largest clique
-l = lGetTopVertices(oGr)
+l = lGetTopVertices(oGr, iQuantile = 0.90)
 # top genes based on centrality parameters
-df = f_dfGetGeneAnnotation(l$degree)
-f_dfGetGeneAnnotation(l$hub)
-f_dfGetGeneAnnotation(l$betweenness)
-f_dfGetGeneAnnotation(l$closeness)
+dfClique = f_dfGetGeneAnnotation(l$clique)
+cvSum = f_csGetGeneSummaryFromGenbank(dfClique$ENTREZID)
+cvSum.2 = dfClique$SYMBOL
+dfClique$Summary = cvSum[cvSum.2]
+write.csv(dfClique, file='Temp/dfClique.csv')
+
+dfDegree = f_dfGetGeneAnnotation(l$degree)
+cvSum = f_csGetGeneSummaryFromGenbank(dfDegree$ENTREZID)
+cvSum.2 = dfDegree$SYMBOL
+dfDegree$Summary = cvSum[cvSum.2]
+write.csv(dfDegree, file='Temp/dfDegree.csv')
+
+dfHub = f_dfGetGeneAnnotation(l$hub)
+cvSum = f_csGetGeneSummaryFromGenbank(dfHub$ENTREZID)
+cvSum.2 = dfHub$SYMBOL
+dfHub$Summary = cvSum[cvSum.2]
+write.csv(dfHub, file='Temp/dfHub.csv')
+
+dfBetweenness = f_dfGetGeneAnnotation(l$betweenness)
+cvSum = f_csGetGeneSummaryFromGenbank(dfBetweenness$ENTREZID)
+cvSum.2 = dfBetweenness$SYMBOL
+dfBetweenness$Summary = cvSum[cvSum.2]
+write.csv(dfBetweenness, file='Temp/dfBetweenness.csv')
+
+dfCloseness = f_dfGetGeneAnnotation(l$closeness)
+cvSum = f_csGetGeneSummaryFromGenbank(dfCloseness$ENTREZID)
+cvSum.2 = dfCloseness$SYMBOL
+dfCloseness$Summary = cvSum[cvSum.2]
+write.csv(dfCloseness, file='Temp/dfCloseness.csv')
 
 
 # plot largest connected graph - clique
 set.seed(1)
 ig = plot.graph.clique(oGr)
+ig = f_igCalculateVertexSizesAndColors(ig, t(mCounts), fGroups, bColor = F, iSize = 20)
+par(mar=c(1,1,1,1)+0.1)
+set.seed(1)
+plot(ig, vertex.label=NA, layout=layout_with_fr, vertex.frame.color=NA, edge.color='lightgrey')
+
 # plot the largest clique
 par(mar=c(1,1,1,1)+0.1)
 ig = induced_subgraph(getFinalGraph(oGr), vids = unlist(getLargestCliques(oGr)))
@@ -94,69 +124,99 @@ plot(ig, layout=layout_with_fr)
 legend('topright', legend = c('Underexpressed', 'Overexpressed'), fill = c('lightblue', 'pink'))
 par(p.old)
 
-# get the genes of importance based on graph properties
-# largest clique
-n = getLargestCliques(oGr)
-n = names(unlist(n))
-f_dfGetGeneAnnotation(cvEnterezID = n)
+# plot the graphs of centrality parameter genes 
+par(mar=c(1,1,1,1)+0.1)
+ig = induced_subgraph(getFinalGraph(oGr), vids = dfDegree$ENTREZID)
+ig = f_igCalculateVertexSizesAndColors(ig, t(mCounts), fGroups, bColor = T)
+n = V(ig)$name
+lab = f_dfGetGeneAnnotation(n)
+V(ig)$label = as.character(lab$SYMBOL)
+set.seed(1)
+plot(ig, vertex.label.cex=0.5, layout=layout_with_fr, vertex.frame.color=NA, edge.color='lightgrey', main='Degree')
+legend('topright', legend = c('Underexpressed', 'Overexpressed'), fill = c('lightblue', 'pink'))
 
-# get the centrality parameters
-mCent = mPrintCentralitySummary(oGr)
-# top  5% of the vertices from each category and largest clique
-l = lGetTopVertices(oGr)
-# top genes based on centrality parameters
-f_dfGetGeneAnnotation(l$degree)
-f_dfGetGeneAnnotation(l$hub)
-f_dfGetGeneAnnotation(l$betweenness)
-f_dfGetGeneAnnotation(l$closeness)
+ig = induced_subgraph(getFinalGraph(oGr), vids = dfCloseness$ENTREZID)
+ig = f_igCalculateVertexSizesAndColors(ig, t(mCounts), fGroups, bColor = T)
+n = V(ig)$name
+lab = f_dfGetGeneAnnotation(n)
+V(ig)$label = as.character(lab$SYMBOL)
+set.seed(1)
+plot(ig, vertex.label.cex=0.5, layout=layout_with_fr, vertex.frame.color=NA, edge.color='lightgrey', main='Closeness')
+legend('topright', legend = c('Underexpressed', 'Overexpressed'), fill = c('lightblue', 'pink'))
+
+ig = induced_subgraph(getFinalGraph(oGr), vids = dfBetweenness$ENTREZID)
+ig = f_igCalculateVertexSizesAndColors(ig, t(mCounts), fGroups, bColor = T)
+n = V(ig)$name
+lab = f_dfGetGeneAnnotation(n)
+V(ig)$label = as.character(lab$SYMBOL)
+set.seed(1)
+plot(ig, vertex.label.cex=0.5, layout=layout_with_fr, vertex.frame.color=NA, edge.color='lightgrey', main='Betweenness')
+legend('topright', legend = c('Underexpressed', 'Overexpressed'), fill = c('lightblue', 'pink'))
+
+ig = induced_subgraph(getFinalGraph(oGr), vids = dfHub$ENTREZID)
+ig = f_igCalculateVertexSizesAndColors(ig, t(mCounts), fGroups, bColor = T)
+n = V(ig)$name
+lab = f_dfGetGeneAnnotation(n)
+V(ig)$label = as.character(lab$SYMBOL)
+set.seed(1)
+plot(ig, vertex.label.cex=0.5, layout=layout_with_fr, vertex.frame.color=NA, edge.color='lightgrey', main='Hub')
+legend('topright', legend = c('Underexpressed', 'Overexpressed'), fill = c('lightblue', 'pink'))
 par(p.old)
-plot.centrality.diagnostics(oGr)
 
 ## we can look at the problem from the other direction and look at clusters instead of genes
 # sample plots
 # mean expression of groups in every cluster
 plot.mean.expressions(oGr, t(mCounts), fGroups, legend.pos = 'bottomleft', main='Total Change in Each Cluster')
 # only significant clusters
-plot.significant.expressions(oGr, t(mCounts), fGroups, main='Significant Clusters', lwd=2, bStabalize = T)
-
+par(mar=c(7, 3, 2, 2)+0.1)
+plot.significant.expressions(oGr, t(mCounts), fGroups, main='Significant Clusters', lwd=2, bStabalize = T, cex.axis=0.7)
+pr.out = plot.components(oGr, t(mCounts), fGroups, bStabalize = T)
+par(mar=c(4,2,4,2))
+biplot(pr.out, cex=0.8, cex.axis=0.8, arrow.len = 0)
 # plot summary heatmaps
 # expression in all clusters
 plot.heatmap.all(oGr, t(mCounts))
 # marginal expression level in each cluster
 plot.heatmap.marginal(oGr, t(mCounts))
-# plot selected clusters
-plot.heatmap.cluster(oGr, t(mCounts), csClustLabel = '1280215')
-plot.heatmap.cluster(oGr, t(mCounts), csClustLabel = '168249')
-plot.heatmap.cluster(oGr, t(mCounts), csClustLabel = '3247509')
-plot.heatmap.cluster(oGr, t(mCounts), csClustLabel = '72203')
+# # plot selected clusters
+# plot.heatmap.cluster(oGr, t(mCounts), csClustLabel = '1280215')
+# plot.heatmap.cluster(oGr, t(mCounts), csClustLabel = '168249')
+# plot.heatmap.cluster(oGr, t(mCounts), csClustLabel = '3247509')
+# plot.heatmap.cluster(oGr, t(mCounts), csClustLabel = '72203')
 
-# plot PCA on components of clusters
-# with some variance stabalization
-pr.out = plot.components(oGr, t(mCounts), fGroups, bStabalize = T)
-par(mar=c(4,2,4,2))
-biplot(pr.out, cex=0.8, cex.axis=0.8, arrow.len = 0)
 
 # get clusters of choice to make subgraphs
 dfCluster = getClusterMapping(oGr)
 colnames(dfCluster) = c('gene', 'cluster')
 dfCluster = dfCluster[order(dfCluster$cluster),]
 
-# plot the subgraphs of the 4 top significant clusters
-l = getSignificantClusters(oGr, mCounts = t(mCounts), fGroups)
-csClust = rownames(l$clusters)[1:6]
+# plot the subgraphs of the significant clusters
+l = getSignificantClusters(oGr, mCounts = t(mCounts), fGroups, bStabalize = T)
+csClust = rownames(l$clusters)
+pdf('Temp/graphs.pdf')
 par(mar=c(1,1,1,1)+0.1)
 sapply(seq_along(csClust), function(x){
   set.seed(1)
   ig.sub = getClusterSubgraph(oGr, csClustLabel = csClust[x])
   ig.sub = f_igCalculateVertexSizesAndColors(ig.sub, t(mCounts), fGroups, bColor = T)
-  plot(ig.sub, vertex.label=NA, layout=layout_with_fr, main=csClust[x])
+  n = f_dfGetGeneAnnotation(V(ig.sub)$name)
+  V(ig.sub)[n$ENTREZID]$label = n$SYMBOL
+  plot(ig.sub, vertex.label.cex=0.5, layout=layout_with_fr, main=csClust[x])
   ig.sub = getLargestCliqueInCluster(oGr, csClustLabel = csClust[x])
   ig.sub = f_igCalculateVertexSizesAndColors(ig.sub, t(mCounts), fGroups, bColor = T)
   n = f_dfGetGeneAnnotation(V(ig.sub)$name)
   V(ig.sub)[n$ENTREZID]$label = n$SYMBOL
   plot(ig.sub, vertex.label.cex=0.8, layout=layout_with_fr, main=csClust[x])
+  plot.heatmap.cluster(oGr, t(mCounts), csClustLabel = csClust[x])
 })
+sym = f_dfGetGeneAnnotation(as.character(dfCluster$gene))
+cluster = as.character(dfCluster$cluster)
+dfCluster = cbind(sym, cluster)
+cvSum = f_csGetGeneSummaryFromGenbank(iID = as.character(dfCluster$ENTREZID))
+cvSum.2 = as.character(dfCluster$SYMBOL)
+dfCluster$Summary = cvSum[cvSum.2]
 
+write.csv(dfCluster, file='Temp/dfCluster.csv')
 
 # saving graph object to visualize in cytoscape or other graph viewers
 ig = getFinalGraph(oGr)
