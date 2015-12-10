547a4bfb6f1c8fc4d8c8d161458a8ca860cd904b
uhkniazi <uhkniazi@users.noreply.github.com>
2015-12-10 11:16:16 +0000

    getSignificantClusters - p.adjust changed to bonferroni from BH

--- CGraphClust.R
+++ CGraphClust.R
@@ -881,7 +881,7 @@ setMethod('getSignificantClusters', signature='CGraphClust', definition = functi
   #fSig = apply(p.vals, 2, function(x) any(x < 0.01))
   p.val = apply(mCent, 1, function(x) anova(lm(x ~ fGroups))$Pr[1])
   #p.val = apply(mCent, 1, function(x) oneway.test(x ~ fGroups)$p.value)
-  p.val = p.adjust(p.val, method = 'BH')
+  p.val = p.adjust(p.val, method = 'bonferroni')
   fSig = p.val < 0.01
   mCent = mCent[fSig,]
   p.val = p.val[fSig]
