15cd35039180a12ad7473781e71a7b4265629f6d
uhkniazi <uhkniazi@users.noreply.github.com>
2016-03-18 13:47:08 +0000

    changed p.adj cutoff to 0.05 from 0.1 in sepsis survivors dataset

--- Test_data/generate_test_data.R
+++ Test_data/generate_test_data.R
@@ -375,7 +375,7 @@ names(lSigGenes.adj) = levels(fSamples)[2:length(levels(fSamples))]
 
 for (i in 2:length(levels(fSamples))){
   p.adj = p.adjust(fit$p.value[,i], method = 'BH')
-  lSigGenes.adj[[i-1]] = names(p.adj)[p.adj < 0.1]
+  lSigGenes.adj[[i-1]] = names(p.adj)[p.adj < 0.05]
 }
 
 #cvSigGenes.adj = unique(cvSigGenes.adj)
